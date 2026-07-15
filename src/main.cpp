bb238349f6eafeb5f67b692eefc41e2cbdd7980b
André Babone de Assunção <baboneandre@gmail.com>
2026-07-15 13:36:56 -0300

    Add calculator menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int option;
+
+    std::cout << "=========================\n";
+    std::cout << "       CALCULATOR\n";
+    std::cout << "=========================\n";
+    std::cout << "1 - Addition\n";
+    std::cout << "2 - Subtraction\n";
+    std::cout << "3 - Multiplication\n";
+    std::cout << "4 - Division\n";
+    std::cout << "0 - Exit\n";
+    std::cout << "Choose an option: ";
+
+    std::cin >> option;
+
+    std::cout << "Selected option: " << option << '\n';
+
     return 0;
 }
